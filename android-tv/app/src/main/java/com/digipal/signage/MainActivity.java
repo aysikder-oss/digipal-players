2a0b400961107593c87a2f2cf195bc7feb821135
aysikder-oss <aysikder@gmail.com>
2026-07-05 13:16:43 -0400

    Fix missing android.util.Log import (compile error from mount watchdog logging)

--- android-tv/app/src/main/java/com/digipal/signage/MainActivity.java
+++ android-tv/app/src/main/java/com/digipal/signage/MainActivity.java
@@ -34,6 +34,7 @@ import android.view.PixelCopy;
 import java.io.ByteArrayOutputStream;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
   import android.text.format.Formatter;
   public class MainActivity extends Activity {
 
