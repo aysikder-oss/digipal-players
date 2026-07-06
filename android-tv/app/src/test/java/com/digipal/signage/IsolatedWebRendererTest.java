c3e455c66ef81d89f208fb134ab8b162a609df7c
aysikder-oss <aysikder@gmail.com>
2026-07-05 21:33:34 -0400

    Fix Android TV hardening: Room DB nested-class, UrlPolicy, tests, Sentry gating (android-tv/app/src/test/java/com/digipal/signage/IsolatedWebRendererTest.java)

--- android-tv/app/src/test/java/com/digipal/signage/IsolatedWebRendererTest.java
+++ android-tv/app/src/test/java/com/digipal/signage/IsolatedWebRendererTest.java
@@ -1,31 +1,35 @@
 package com.digipal.signage;
 
-  import static org.junit.Assert.assertEquals;
-  import org.junit.Test;
+    import static org.junit.Assert.assertEquals;
 
-  public class IsolatedWebRendererTest {
-      @Test
-      public void appendQueryParameterPreservesFragment() {
-          String out = IsolatedWebRenderer.appendQueryParameter(
-                  "https://www.digipalsignage.com/tv/render/design?id=1#preview",
-                  "renderToken",
-                  "123");
+    import org.junit.Test;
+    import org.junit.runner.RunWith;
+    import org.robolectric.RobolectricTestRunner;
 
-          assertEquals(
-                  "https://www.digipalsignage.com/tv/render/design?id=1&renderToken=123#preview",
-                  out);
-      }
+    @RunWith(RobolectricTestRunner.class)
+    public class IsolatedWebRendererTest {
+        @Test
+        public void appendQueryParameterPreservesFragment() {
+            String out = IsolatedWebRenderer.appendQueryParameter(
+                    "https://www.digipalsignage.com/tv/render/design?id=1#preview",
+                    "renderToken",
+                    "123");
 
-      @Test
-      public void appendQueryParameterWorksWithoutQuery() {
-          String out = IsolatedWebRenderer.appendQueryParameter(
-                  "https://www.digipalsignage.com/tv/render/design#preview",
-                  "renderToken",
-                  "123");
+            assertEquals(
+                    "https://www.digipalsignage.com/tv/render/design?id=1&renderToken=123#preview",
+                    out);
+        }
 
-          assertEquals(
-                  "https://www.digipalsignage.com/tv/render/design?renderToken=123#preview",
-                  out);
-      }
-  }
-  
+        @Test
+        public void appendQueryParameterWorksWithoutQuery() {
+            String out = IsolatedWebRenderer.appendQueryParameter(
+                    "https://www.digipalsignage.com/tv/render/design#preview",
+                    "renderToken",
+                    "123");
+
+            assertEquals(
+                    "https://www.digipalsignage.com/tv/render/design?renderToken=123#preview",
+                    out);
+        }
+    }
+    
