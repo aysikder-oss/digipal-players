52a63e7f7fe8f87acca3e11f3b3ce77924f27745
aysikder-oss <aysikder@gmail.com>
2026-07-15 13:49:27 -0400

    Copy android-tv to amazon fire tv (android): app/src/main/java/com/digipal/signage/RecyclingSafeImageView.java

---
+++ amazon fire tv (android)/app/src/main/java/com/digipal/signage/RecyclingSafeImageView.java
@@ -0,0 +1,41 @@
+package com.digipal.signage;
+
+  import android.content.Context;
+  import android.graphics.Canvas;
+  import android.util.AttributeSet;
+  import android.util.Log;
+  import androidx.appcompat.widget.AppCompatImageView;
+
+  public class RecyclingSafeImageView extends AppCompatImageView {
+      private static final String TAG = "DigipalImageView";
+
+      public RecyclingSafeImageView(Context context) {
+          super(context);
+      }
+
+      public RecyclingSafeImageView(Context context, AttributeSet attrs) {
+          super(context, attrs);
+      }
+
+      public RecyclingSafeImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+          super(context, attrs, defStyleAttr);
+      }
+
+      @Override
+      protected void onDraw(Canvas canvas) {
+          try {
+              super.onDraw(canvas);
+          } catch (RuntimeException e) {
+              Log.w(TAG, "onDraw suppressed (recycled bitmap): " + e.getMessage());
+          }
+      }
+
+      @Override
+      protected void onDetachedFromWindow() {
+          try {
+              setImageDrawable(null);
+          } catch (Throwable ignored) {}
+          super.onDetachedFromWindow();
+      }
+  }
+  
